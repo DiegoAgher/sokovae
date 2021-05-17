ba87e8519e6827777a8014fd2f37434fdb7cad8b
DiegoAgher <aguadod@gmail.com>
2021-05-17 16:19:47 +0200

    add deeper encoder and decoder for 40x40 imgs

--- models/autoencoders.py
+++ models/autoencoders.py
@@ -10,14 +10,15 @@ class BaseVariational(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, x):
+    def forward(self, x, debug=False):
         for layer in self.layers:
             x = layer(x)
             # if isinstance(layer, nn.Flatten):
             #     print(x.shape)
-            # print("layer {}".format(layer))
-            # print("shape {}".format(x.shape))
-            # print("")
+            if debug:
+                print("layer {}".format(layer))
+                print("shape {}".format(x.shape))
+                print("")
         return x
 
     def predict(self, x, num_forward_passes=10):
@@ -38,6 +39,52 @@ class BaseVariational(torch.nn.Module):
                 kl_ = layer.kl_divergence()
                 kl += kl_
         return kl
+
+
+class DeepEncoderSmall(BaseVariational):
+    '''
+    Takes in as input batches of images of size [batch_size, 40, 40, 3]
+    '''
+    def __init__(self, latent_size=9):
+        super().__init__()
+        self.latent_size = latent_size
+        self.layers = torch.nn.ModuleList(self._init_layers())
+
+    def _init_layers(self):
+        layers = [
+                Conv2d(in_channels=3, out_channels=32,
+                      kernel_size=3, stride=2, padding=1),
+                ReLU(),
+                Conv2d(in_channels=32, out_channels=64,
+                          kernel_size=3, stride=2, padding=1),
+                ReLU(),
+                Conv2d(in_channels=64, out_channels=128,
+                          kernel_size=3, stride=2, padding=1),
+                BatchNorm2d(128),
+                ReLU(),
+                Conv2d(in_channels=128, out_channels=256,
+                          kernel_size=3, stride=2, padding=1),
+                ReLU(),
+                Conv2d(in_channels=256, out_channels=512,
+                          kernel_size=3, stride=1, padding=1),
+                BatchNorm2d(512),
+                ReLU(),
+                Conv2d(in_channels=512, out_channels=1024,
+                          kernel_size=3, stride=1, padding=1),
+                BatchNorm2d(1024),
+                ReLU(),
+                Conv2d(in_channels=1024, out_channels=1024,
+                          kernel_size=3, stride=3, padding=4),
+                BatchNorm2d(1024),
+                ReLU(),
+                Dropout2d(p=0.25),
+                Flatten(),
+                Dropout(p=0.4),
+                Linear(9216, int(9216 / 9)),
+                ReLU(),
+                Dropout(p=0.4),
+                BayesianLayer(int(9216 / 9), self.latent_size)]
+        return layers
 
 
 class EncoderSmall(BaseVariational):
@@ -80,6 +127,84 @@ class EncoderSmall(BaseVariational):
                 Dropout(p=0.4),
                 BayesianLayer(int(9216 / 9), self.latent_size)]
         return layers
+
+class DeepDecoderSmall(BaseVariational):
+    '''
+    Use with SmallDecoder.
+    will take a Dense vector and turn it into batches of size [batch_size, 40, 40, 3]
+    '''
+    def __init__(self, latent_size):
+        super().__init__()
+        self.latent_size = latent_size
+        self.layers = torch.nn.ModuleList(self._init_layers())
+
+    def _init_layers(self):
+        self.start_decode = Linear(self.latent_size, int(9216 / 9))
+        self.drop_linear_one = Dropout(p=0.4)
+        self.start_decodeb = Linear(int(9216 / 9), 9216)
+        self.drop_linear_two = Dropout(p=0.4)
+        layers = [
+                ConvTranspose2d(in_channels=1024, out_channels=1024,
+                          kernel_size=3, stride=3, padding=3),
+                BatchNorm2d(1024),
+                Dropout2d(p=0.25),
+                ReLU(),
+                ConvTranspose2d(in_channels=1024, out_channels=1024,
+                          kernel_size=3, stride=3, padding=3),
+                BatchNorm2d(1024),
+                Dropout2d(p=0.25),
+                ReLU(),
+                ConvTranspose2d(in_channels=1024, out_channels=512,
+                          kernel_size=3, stride=2, padding=1, output_padding=1),
+                BatchNorm2d(512),
+                Dropout2d(p=0.25),
+                ReLU(),
+                ConvTranspose2d(in_channels=512, out_channels=256,
+                          kernel_size=3, stride=2, padding=1, output_padding=1),
+                BatchNorm2d(256),
+                ReLU(),
+                ConvTranspose2d(in_channels=256, out_channels=128,
+                          kernel_size=3, stride=2, padding=1),
+                BatchNorm2d(128),
+                ReLU(),
+                ConvTranspose2d(in_channels=128, out_channels=64,
+                      kernel_size=3, stride=2, padding=2),
+                BatchNorm2d(64),
+                ReLU(),
+                ConvTranspose2d(in_channels=64, out_channels=32,
+                          kernel_size=3, stride=1, padding=1),
+                BatchNorm2d(32),
+                ReLU(),
+                ConvTranspose2d(in_channels=32, out_channels=16,
+                                          kernel_size=3, stride=1, padding=1),
+                ReLU(),
+                BatchNorm2d(16),
+                Conv2d(in_channels=16, out_channels=8,
+                          kernel_size=4, stride=1, padding=1),
+                ReLU(),   
+                Conv2d(in_channels=8, out_channels=3,
+                          kernel_size=3, stride=1, padding=0),
+                Tanh()    
+            ]
+        return layers
+    
+    def forward(self, x, debug=False):
+        x = self.start_decode(x)
+        x = self.drop_linear_one(x)
+        x = F.relu(x)
+        x = self.start_decodeb(x)
+        x = self.drop_linear_two(x)
+        x = F.relu(x)
+        x = x.reshape(-1, 1024, 3, 3)
+
+        for idx, layer in enumerate(self.layers):
+            x = layer(x)
+            if debug:
+                print("layer {}".format(layer))
+                print("shape {}".format(x.shape))
+                print("")
+        return x
+
 
 
 class DecoderSmall(BaseVariational):
