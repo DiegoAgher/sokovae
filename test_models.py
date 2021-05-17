d752208368104575baaebcf5cab2c15837dcc7bf
DiegoAgher <aguadod@gmail.com>
2021-05-17 16:26:03 +0200

    add 'tests' for models

--- test_models.py
+++ test_models.py
@@ -1,7 +1,19 @@
 from models.autoencoders import EncoderSmall, DecoderSmall, CnnAE
+from models.autoencoders import DeepEncoderSmall, DeepDecoderSmall
 
 encoder = EncoderSmall(9)
 decoder = DecoderSmall(9)
+ae = CnnAE(encoder, decoder)
+
+
+print(encoder)
+print(decoder)
+print(ae)
+
+
+
+encoder = DeepEncoderSmall(9)
+decoder = DeepDecoderSmall(9)
 ae = CnnAE(encoder, decoder)
 
 
