d491a640940e08965af47ba4304afb2ee6d7231b
DiegoAgher <aguadod@gmail.com>
2021-05-18 12:57:52 +0200

    update ghost mask

--- Train/loss_functions.py
+++ Train/loss_functions.py
@@ -12,7 +12,6 @@ def build_mask_np(observation, pen=1):
           mask *= 1000
           mask += 1
     return green_mask, red_mask
-
 def build_mask(observation):
     green_mask = torch.zeros_like(observation[0])
     green_mask[observation[1,:, :] > -0.3] = 1
@@ -28,18 +27,23 @@ def build_mask(observation):
     boxes_mask = green_mask * red_mask
 
     for mask in [ghost_mask, boxes_mask]:
+        mask *= 10
         mask += 1
-        mask *= 2
     return ghost_mask, boxes_mask
 
-def make_small_objects_important(reconstructed, image):
+def make_small_objects_important(reconstructed, image, only_ghost=True):
     diff = (reconstructed - image)
 
     for idx, observation in enumerate(image):
         ghost_mask, boxes_mask = build_mask(observation)
         
+        masks_to_apply = ghost_mask.clone()
+        if not only_ghost:
+          print("should not print this")
+          masks_to_apply += boxes_mask
+
         for ch_id in range(3):
-          diff[idx, ch_id, :, :] = diff[idx, ch_id, :, :] * (ghost_mask + boxes_mask)
+          diff[idx, ch_id, :, :] = diff[idx, ch_id, :, :] * masks_to_apply
         
     diff = diff **2
     return torch.mean(diff)
